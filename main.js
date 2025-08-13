4511d7684c67aae3abc92740a277f92a65ea5c3d
Carmen Tenorio <carmen.tenorio.btsoft@gmail.com>
2025-08-13 18:28:00 -0400

    layout, and button and checkbox functionality

--- main.js
+++ main.js
@@ -1,92 +1,100 @@
-const input = document.querySelector("input");
+const inputTask = document.querySelector("#taskInput");
+const inputDesc = document.querySelector("#descriptionInput");
 const addBtn = document.querySelector(".btn-add");
 const ul = document.querySelector("ul");
 const empty = document.querySelector(".empty");
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-render();
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+//descriptions of tasks
+//let descriptions = JSON.parse(localStorage.getItem("descriptions")) || [];
+//array para guardar en checked
+//let completed = JSON.parse(localStorage.getItem("completed")) || [];
+/**
+ * load tasks on page startup
+ */
+render();
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
-
-  const taskText = input.value.trim();
-
-  if (taskText !== "") {
-    addTaskToList(taskText);
-    input.value = "";
-    empty.style.display = "none";
+  //console.log(inputDesc,inputTask);
+  const taskText = inputTask.value.trim();
+  const description = inputDesc.value.trim();
+  if (taskText !== "" && description !== "") {
+    addTaskToList(taskText, description);
+    inputTask.value = "";
+    inputDesc.value = "";
+    empty.style.display = "none"; //ocultar mensaje de lista vacia
   }
 });
-
-function deleteTask(text) {
-  deleteTask(text);
-  render();
-}
-
-function renderTask(taskText) {
-  const li = document.createElement("li");
-  const p = document.createElement("p");
-  //ul.innerHTML = `<li><p>${taskText}</p> <button onclick=deleteTask(${taskText})></button></li>`;
-  //ul.innerHTML = `<li><p>${taskText}</p> <button onclick="deleteTask('${taskText}')"></button></li>`;
-
-  p.textContent = taskText;
-  const deleteBtn = addDeleteBtn(taskText);
-  deleteBtn.addEventListener("click", () => {
-    removeTaskFromList(taskText, li);
-  });
-
-  li.appendChild(p);
-  li.appendChild(deleteBtn);
-  ul.appendChild(li);
-}
 /**
- * Create the delete button using your function and passing the task to it
- * @param {*} taskText
+ * function to render and display dynamically
  * @returns
  */
-function addDeleteBtn(taskText) {
-  const deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("btn-delete");
-  const icon = document.createElement("i");
-  icon.classList.add("fa-solid", "fa-trash"); //clase del icon
-  icon.style.cursor = "pointer";
-  icon.style.color = "red";
-  deleteBtn.appendChild(icon);
-  deleteBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    deleteTask(taskText);
-    render();
-  });
-  deleteBtn.appendChild(icon);
-  return deleteBtn;
-}
-/**
- * Load tasks on startup
- *
- */
 function render() {
-  tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  console.log("Lista completa de tareas:", tasks);
-  ul.innerHTML = "";
+  if (tasks.length === 0) {
+    ul.innerHTML = "";
+    empty.style.display = "block";
+    return;
+  }
+  empty.style.display = "none";
 
-  tasks.forEach((task) => {
-    renderTask(task);
-  });
-  empty.style.display = tasks.length === 0 ? "block" : "none";
+  ul.innerHTML = tasks
+    .map((task, index) => {
+      return `
+      <li>
+       <input type="checkbox"
+       ${task.completed ? "checked" : ""}
+        onchange="toggleTask(${index})">
+        <p>${task.text} - ${task.description} </p>
+        <button class="btn-view" onclick="viewTaskByIndex(${index})">
+          <i class="fa-solid fa-eye" style="cursor:pointer; color:green;"></i>
+        </button>
+        <button class="btn-edit" onclick="editTaskByIndex(${index})">
+          <i class="fa-solid fa-pen-to-square" style="cursor:pointer; color:blue;"></i>
+        </button>
+         <button class="btn-delete" onclick="deleteTaskByIndex(${index})">
+          <i class="fa-solid fa-trash" style="cursor:pointer; color:red;"></i>
+        </button>
+      </li>
+    `;
+    })
+    .join("");
+}
+
+function toggleTask(index) {
+  //este hace que sea aleatorio el checked o no checked
+  tasks[index].completed = !tasks[index].completed;
+  saveTasksToStorage();
+  render();
 }
 /**
- * delete task function
+ * function that does not delete by index in the map
  * @param {*} taskText
  */
 function deleteTask(taskText) {
   tasks = tasks.filter((task) => task !== taskText);
   saveTasksToStorage();
+  render();
 }
 /**
- * Add task to array and DOM
- * @param {*} taskText
+ * add task to DOM and array
+ * @param {*} taskText, description
  */
-function addTaskToList(taskText) {
-  tasks.push(taskText);
+function addTaskToList(taskText, description) {
+  tasks.push({
+    text: taskText,
+    description: description,
+    completed: false,
+  });
+  saveTasksToStorage();
+  render();
+}
+/**
+ * function that doses not delete by index
+ * @param {*} i
+ */
+function deleteTaskByIndex(i) {
+  //splice modified the content of array, delete element of the position
+  tasks.splice(i, 1);
   saveTasksToStorage();
   render();
 }
@@ -96,28 +104,92 @@ function addTaskToList(taskText) {
  * @param {*} liElement
  */
 function removeTaskFromList(taskText, liElement) {
-  tasks = tasks.filter((task) => task !== taskText);
-  saveTasksToStorage();
-
-  if (tasks.length === 0) {
-    empty.style.display = "block";
-  }
+  deleteTask(taskText);
 }
 /**
- * Save to localStorage
+ * save in localStorage
  */
 function saveTasksToStorage() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+/**
+ * view task (only shows text in alert)
+ * @param {*} id
+ */
+function viewTaskByIndex(index) {
+  const task = tasks[index];
+  if (task) {
+    alert("Task: " + task.text + "\nDescription: " + task.description);
+  }
+}
+
+/**
+ * edit task by id
+ * @param {*} id
+ */
+function editTaskByIndex(index) {
+  const newText = prompt("Edit task:", tasks[index].text);
+  const newDesc = prompt("Edit description:", tasks[index].description);
+
+  if (newText && newText.trim() !== "") {
+    tasks[index].text = newText.trim();
+  }
+  if (newDesc && newDesc.trim() !== "") {
+    tasks[index].description = newDesc.trim();
+  }
+
+  saveTasksToStorage();
+  render();
+}
+
+/**
+ * function that is no longer used since render contains it
+ * @param {*} taskText
+ */
+function renderTask(taskText) {
+  const li = document.createElement("li");
+  const p = document.createElement("p");
+  //ul.innerHTML = `<li><p>${taskText}</p> <button onclick=deleteTask(${taskText})></button></li>`;
+  //ul.innerHTML = `<li><p>${taskText}</p> <button onclick="deleteTask('${taskText}')"></button></li>`;
+  p.textContent = taskText;
+  // Crear el botón eliminar usando tu función y pasándole la tarea
+  const deleteBtn = addDeleteBtn(taskText);
+  li.appendChild(p);
+  li.appendChild(deleteBtn);
+  ul.appendChild(li);
+}
+/**
+ * function used in function renderTask
+ * @param {*} taskText
+ * @returns
+ */
+
+function addDeleteBtn(taskText) {
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("btn-delete");
+
+  const icon = document.createElement("i");
+  icon.classList.add("fa-solid", "fa-trash"); //clase del icon
+  icon.style.cursor = "pointer";
+  icon.style.color = "red";
+
+  //Insertar ícono dentro del botón
+  deleteBtn.appendChild(icon);
+
+  deleteBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    deleteTask(taskText);
+    render();
+  });
+  deleteBtn.appendChild(icon);
+  return deleteBtn;
 }
 
 function editBtn() {
   const editBtn = document.createElement("button");
   editBtn.classList.add("btn-edit");
-  //crear icono editar
   const icon = document.createElement("i");
-  icon.classList.add("fa-solid", "fa-pen-to-square"); //clase del icon
-
-  //Insertar ícono dentro del botón
+  icon.classList.add("fa-solid", "fa-pen-to-square");
   editBtn.appendChild(icon);
 
   editBtn.addEventListener("click", (e) => {
@@ -135,11 +207,9 @@ function editBtn() {
 function viewBtn() {
   const viewBtn = document.createElement("button");
   viewBtn.classList.add("btn-edit");
-  //crear icono ver
   const icon = document.createElement("i");
-  icon.classList.add("fa-solid", "fa-eye"); //clase del icon
+  icon.classList.add("fa-solid", "fa-eye");
 
-  //Insertar ícono dentro del botón
   viewBtn.appendChild(icon);
 
   viewBtn.addEventListener("click", (e) => {
@@ -151,6 +221,5 @@ function viewBtn() {
   });
 
   viewBtn.appendChild(icon);
-  ~1;
   return viewBtn;
 }
